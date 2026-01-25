8c2817cd5efe1d44fa5e76a1c78bc8ec62beef24
Andydecori <anguerreroal@uide.edu.ec>
2026-01-24 19:07:21 -0500

    Se agrega el Proyecto

---
+++ go.mod
@@ -0,0 +1,3 @@
+module LibrosElectronicosGolang
+
+go 1.25.5
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hola, Aqui va mi sistema de gestion de libros electronicos")
+}
